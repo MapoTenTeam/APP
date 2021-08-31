61934faa70d71b468a8c37b68594657ed72435d9
JiyoonChae <71802752+JiyoonChae@users.noreply.github.com>
2021-08-31 16:00:26 +0900

    add 기업마이페이지 및 하위 프레그먼트들

---
+++ MapoTen/app/src/main/java/com/mapo/mapoten/ui/fragment/BusinessAccount_01_01.kt
@@ -0,0 +1,23 @@
+package com.mapo.mapoten.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mapo.mapoten.R
+
+
+class BusinessAccount_01_01 : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_business_account_01_01, container, false)
+    }
+
+
+}
