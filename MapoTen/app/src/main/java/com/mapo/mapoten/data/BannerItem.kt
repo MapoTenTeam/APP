8111496bf82ca43eb5e5aae7e795b2d598777976
KHJ-11 <72050086+KHJ-11@users.noreply.github.com>
2021-09-06 17:21:22 +0900

    0906

---
+++ MapoTen/app/src/main/java/com/mapo/mapoten/data/BannerItem.kt
@@ -0,0 +1,11 @@
+package com.mapo.mapoten.data
+
+/**
+ * @author hj
+ * @email syk01132@gmail.com
+ * @created 2021-09-06
+ * @desc
+ */
+data class BannerItem(
+    val homeImage: Int
+)
---
+++ MapoTen/app/src/main/java/com/mapo/mapoten/ui/adapter/HomeViewPagerAdapter.kt
@@ -0,0 +1,40 @@
+package com.mapo.mapoten.ui.adapter
+
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.mapo.mapoten.R
+import com.mapo.mapoten.data.BannerItem
+
+/**
+ * @author hj
+ * @email syk01132@gmail.com
+ * @created 2021-09-06
+ * @desc
+ */
+class HomeViewPagerAdapter(private val bannerItemList:ArrayList<BannerItem>)
+    : RecyclerView.Adapter<HomeViewPagerAdapter.BannerHolder>() {
+
+    inner class BannerHolder(rowRoot: View) : RecyclerView.ViewHolder(rowRoot) {
+        val bannerImage : ImageView = rowRoot.findViewById(R.id.iv_bannerItem)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BannerHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_layout_home_banner,parent,false)
+        return BannerHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: BannerHolder, position: Int) {
+        val bannerData = bannerItemList[position%5]
+        with(holder) {
+            bannerImage.setImageResource(bannerData.homeImage)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return Int.MAX_VALUE
+    }
+}
