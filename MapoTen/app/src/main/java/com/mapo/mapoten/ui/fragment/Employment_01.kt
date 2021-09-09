82b31b84fa46519a451060923b86f6fca0a01f71
KHJ-11 <72050086+KHJ-11@users.noreply.github.com>
2021-09-09 13:38:04 +0900

    0909

--- MapoTen/app/src/main/java/com/mapo/mapoten/ui/fragment/Employment_01.kt
+++ MapoTen/app/src/main/java/com/mapo/mapoten/ui/fragment/Employment_01.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import androidx.navigation.Navigation
 import androidx.viewpager2.widget.ViewPager2
@@ -23,6 +25,40 @@ class Employment_01 : Fragment() {
             orientation = ViewPager2.ORIENTATION_HORIZONTAL
         }
 
+        careerSpinner()
+        jobSpinner()
+        countrySpinner()
         return view
+    }
+
+    private fun careerSpinner() {
+        val career = resources.getStringArray(R.array.employ_array_career)
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, career)
+        binding.empSpinnerCareer.adapter = adapter
+    }
+
+    private fun jobSpinner() {
+        val job = resources.getStringArray(R.array.employ_array_job)
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, job)
+        binding.empSpinnerJob.adapter = adapter
+    }
+
+    private fun countrySpinner() {
+        val country = resources.getStringArray(R.array.employ_array_country)
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_dropdown_item, country)
+        binding.empSpinnerCountry.adapter = adapter
+    }
+
+    private fun careerSpinnerHandler() {
+        binding.empSpinnerCareer.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
     }
 }
