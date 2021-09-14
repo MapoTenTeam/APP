d1c09ec788fe8feaba0144a8531fd26b060dbe10
shyunju7 <shyunju7@naver.com>
2021-09-14 13:59:53 +0900

    FIX businessProfile backbutton and STYLE button

--- MapoTen/app/src/main/java/com/mapo/mapoten/ui/fragment/BusinessProfile_01.kt
+++ MapoTen/app/src/main/java/com/mapo/mapoten/ui/fragment/BusinessProfile_01.kt
@@ -5,19 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.mapo.mapoten.R
+import com.mapo.mapoten.databinding.FragmentBusinessProfile01Binding
 import java.io.File
 
 
 class BusinessProfile_01 : Fragment() {
+
+    lateinit var binding: FragmentBusinessProfile01Binding
 
     val file = File("파일의 경로")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_business_profile_01, container, false)
-        // Inflate the layout for this fragment
+        binding = FragmentBusinessProfile01Binding.inflate(inflater, container, false)
+
+        val view = binding.root
+
+        binding.backBtn.setOnClickListener {
+            Navigation.findNavController(view).navigateUp()
+        }
+
         return view
     }
 
