b3a2a29f40441d9cadbc1ec71ba9bfe66fc6abfb
JiyoonChae <71802752+JiyoonChae@users.noreply.github.com>
2021-09-16 17:44:14 +0900

    update BusinessProfile_01.kt

--- MapoTen/app/src/main/java/com/mapo/mapoten/ui/fragment/BusinessAccount_01_02.kt
+++ MapoTen/app/src/main/java/com/mapo/mapoten/ui/fragment/BusinessAccount_01_02.kt
@@ -5,16 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.mapo.mapoten.R
+import com.mapo.mapoten.databinding.FragmentBusinessAccount0102Binding
 
 class BusinessAccount_01_02 : Fragment() {
+
+    lateinit var binding : FragmentBusinessAccount0102Binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_business_account_01_02, container, false)
+        binding= FragmentBusinessAccount0102Binding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.backBtn.setOnClickListener {
+            Navigation.findNavController(view).navigateUp()
+        }
+
+        return view
     }
 
 
--- MapoTen/app/src/main/java/com/mapo/mapoten/ui/fragment/BusinessProfile_01.kt
+++ MapoTen/app/src/main/java/com/mapo/mapoten/ui/fragment/BusinessProfile_01.kt
@@ -1,10 +1,15 @@
 package com.mapo.mapoten.ui.fragment
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation
 import com.mapo.mapoten.R
 import com.mapo.mapoten.databinding.FragmentBusinessProfile01Binding
@@ -12,6 +17,8 @@ import java.io.File
 
 
 class BusinessProfile_01 : Fragment() {
+
+
 
     lateinit var binding: FragmentBusinessProfile01Binding
 
@@ -28,8 +35,50 @@ class BusinessProfile_01 : Fragment() {
             Navigation.findNavController(view).navigateUp()
         }
 
+        initiateLogoUpload()
+
         return view
     }
 
+
+    fun initiateLogoUpload(){
+        binding.iconImageUpload.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    //갤러리에서 사진 선택
+
+                }
+                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)->{
+                    //동의하라는 팝업
+                    showPermissionContextPopup()
+
+                }
+                else -> {
+                    requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1000)
+                }
+            }
+        }
+    }
+
+    private fun showPermissionContextPopup(){
+        AlertDialog.Builder(requireContext())
+            .setTitle("권한이 필요합니다")
+            .setMessage("사진을 불러오기 위해 권한 필요")
+            .setPositiveButton("동의하기",{dialog, which ->
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1000)
+            })
+            .setNegativeButton("취소하기",{_, _ ->})
+            .create()
+            .show()
+    }
+
+    private fun navigatePhotos(){
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, 2000)
+    }
 
 }
