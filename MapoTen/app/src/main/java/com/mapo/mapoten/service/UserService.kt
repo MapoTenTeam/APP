829a1c5240c400c43e0e08deabc9c67b222cfdfb
SoyoungLee <51827047+soyoung-dev@users.noreply.github.com>
2021-09-28 15:50:31 +0900

    feat : retrofit

--- MapoTen/app/src/main/java/com/mapo/mapoten/service/UserService.kt
+++ MapoTen/app/src/main/java/com/mapo/mapoten/service/UserService.kt
@@ -4,9 +4,25 @@ import com.mapo.mapoten.data.DuplicateIdInfoItem
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface UserService {
 
     @GET("user/duplicate/id/{userid}")
     fun isDuplicateUserId(@Path("userid") userId : String) : Call<DuplicateIdInfoItem>
+
+    @GET("user/duplicate/email/{email}")
+    fun isDuplicateUserEmail(@Path("email") email : String) : Call<DuplicateIdInfoItem>
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- MapoTen/app/src/main/java/com/mapo/mapoten/ui/fragment/Login_02_01.kt
+++ MapoTen/app/src/main/java/com/mapo/mapoten/ui/fragment/Login_02_01.kt
@@ -1,5 +1,6 @@
 package com.mapo.mapoten.ui.fragment
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,7 +11,6 @@ import android.widget.Toast
 import com.mapo.mapoten.R
 import com.mapo.mapoten.config.RetrofitBuilder
 import com.mapo.mapoten.data.DuplicateIdInfoItem
-import com.mapo.mapoten.databinding.FragmentLogin0103Binding
 import com.mapo.mapoten.databinding.FragmentLogin0201Binding
 import com.mapo.mapoten.service.UserService
 import retrofit2.Call
@@ -26,44 +26,69 @@ class Login_02_01 : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         _binding = FragmentLogin0201Binding.inflate(inflater, container, false)
 
         userService = RetrofitBuilder.getInstance().create(UserService::class.java)
 
 
-        binding.btnIdDoubleCheck.setOnClickListener {
-            val duplicateId = userService.isDuplicateUserId(binding.idEditText.text.toString())
+        with(binding) {
+            btnIdDoubleCheck.setOnClickListener {
+                val duplicateId = userService.isDuplicateUserId(idEditText.text.toString())
 
-            duplicateId.enqueue(object : Callback<DuplicateIdInfoItem> {
-                override fun onResponse(
-                    call: Call<DuplicateIdInfoItem>,
-                    response: Response<DuplicateIdInfoItem>
-                ) {
-                    if (response.isSuccessful) {
-
-                        Toast.makeText(
-                            context,
-                            "200 ${response.body()?.message}",
-                            Toast.LENGTH_SHORT
-                        )
-                            .show()
-                    } else {
-
-                        Toast.makeText(context, "${response.body()?.message}", Toast.LENGTH_SHORT)
-                            .show()
+                duplicateId.enqueue(object : Callback<DuplicateIdInfoItem> {
+                    override fun onResponse(
+                        call: Call<DuplicateIdInfoItem>,
+                        response: Response<DuplicateIdInfoItem>,
+                    ) {
+                        if (response.isSuccessful) {
+                            Toast.makeText(context, "200 ${response.body()?.message}", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(context, "${response.body()?.message}", Toast.LENGTH_SHORT).show()
+                        }
                     }
-                }
 
-                override fun onFailure(call: Call<DuplicateIdInfoItem>, t: Throwable) {
+                    override fun onFailure(call: Call<DuplicateIdInfoItem>, t: Throwable) {
+                        Log.e("error", "통신 실패" + t.localizedMessage)
+                    }
 
-                    Log.e("error", "통신 실패" + t.localizedMessage)
+                })
+            }
 
-                }
+            btnEmailDoubleCheck.setOnClickListener {
+                Log.d("TAG", "클릭")
+                val duplicateEmail = userService.isDuplicateUserEmail(emailEditText.text.toString())
+                Log.d("TAG", "${emailEditText.text}")
 
-            })
+                duplicateEmail.enqueue(object : Callback<DuplicateIdInfoItem> {
+                    override fun onResponse(
+                        call: Call<DuplicateIdInfoItem>,
+                        response: Response<DuplicateIdInfoItem>,
+                    ) {
+                        if (response.isSuccessful) {
+                            Log.d("TAG", "response : ${response.code()}")
+//                            when(response.code()){
+//                                200-> Toast.makeText(context,"사용가능한 이메일입니다",Toast.LENGTH_SHORT).show()
+//                                201-> Toast.makeText(context,"사용 불가능한 이메일입니다",Toast.LENGTH_SHORT).show()
+//                                400-> Toast.makeText(context,"이메일 형태가 아닙니다.",Toast.LENGTH_SHORT).show()
+//                            }
+                            Toast.makeText(context, " ${response.body()?.message}", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Toast.makeText(context, "${response.body()?.message}", Toast.LENGTH_SHORT).show()
+                            Log.d("TAG", "response : ${response.code()}")
+                        }
+                    }
+
+                    override fun onFailure(call: Call<DuplicateIdInfoItem>, t: Throwable) {
+                        Log.e("error", "통신 실패" + t.localizedMessage)
+                    }
+
+                })
+            }
+
         }
+
 
 
 
