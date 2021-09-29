636a092592054d854a0679da10f7ca3941b31fa7
JiyoonChae <71802752+JiyoonChae@users.noreply.github.com>
2021-09-29 14:59:21 +0900

    개인 회원 정보 수정 retrofit 연결2

--- MapoTen/app/src/main/java/com/mapo/mapoten/service/AccountManageService.kt
+++ MapoTen/app/src/main/java/com/mapo/mapoten/service/AccountManageService.kt
@@ -1,5 +1,6 @@
 package com.mapo.mapoten.service
 
+import com.mapo.mapoten.data.DuplicateIdInfoItem
 import com.mapo.mapoten.ui.data.PersonalProfile
 import com.mapo.mapoten.ui.data.PersonalProfileItems
 import retrofit2.Call
@@ -7,9 +8,18 @@ import retrofit2.http.*
 
 interface AccountManageService {
 
-
     @GET("user/personal/profile")
     fun getUserProfile(): Call<PersonalProfile>
+
+    @FormUrlEncoded
+    @PUT("user/personal/upload/profile")
+    fun updateUserProfile(
+        @Field("MBER_NM") name:String,
+        @Field("MBER_EMAIL_ADRES") email: String,
+        @Field("MBTLNUM") mobile : String,
+        @Field("ADRES") address: String,
+        @Field("DETAIL_ADRES") detailAd : String
+    ):Call<DuplicateIdInfoItem>
 }
 
 
