f53f2a5bcc460191e20a08c736fc3dae10570216
shyunju7 <shyunju7@naver.com>
2021-09-30 16:41:13 +0900

    FEAT : GeneralJobPosting image 연결

--- MapoTen/app/src/main/java/com/mapo/mapoten/ui/adapter/GeneralEmploymentPostingAdapter.kt
+++ MapoTen/app/src/main/java/com/mapo/mapoten/ui/adapter/GeneralEmploymentPostingAdapter.kt
@@ -1,14 +1,17 @@
 package com.mapo.mapoten.ui.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.mapo.mapoten.R
 import com.mapo.mapoten.config.RetrofitBuilder
 import com.mapo.mapoten.data.employment.EmploymentJobPostingItem
@@ -39,20 +42,28 @@ class GeneralEmploymentPostingAdapter(private val context: Context) :
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         private val title: TextView = view.findViewById(R.id.postingTitle)
+        private val companyImage: ImageView = view.findViewById(R.id.companyImage)
         private val companyName: TextView = view.findViewById(R.id.CompanyName)
         private val jobType: TextView = view.findViewById(R.id.jobType)
         private val careerType: TextView = view.findViewById(R.id.careerType)
         private val place: TextView = view.findViewById(R.id.place)
         private val date: TextView = view.findViewById(R.id.date)
 
+        @SuppressLint("SetTextI18n")
         fun bind(item: GeneralEmpPostingDTO) {
             title.text = item.title
+
             companyName.text = item.companyName
             jobType.text = item.jobType
             careerType.text = item.career
             place.text = item.address
             date.text = "모집기간: " + item.startReception + " - " + item.endReception
 
+            if(item.companyImage != null ){
+                Glide.with(context).load(item.companyImage).into(companyImage)
+            }else {
+                companyImage.setImageResource(R.drawable.banner_image1)
+            }
 
             itemView.setOnClickListener {
                 val bundle = bundleOf("title" to title.text, "date" to date.text)
