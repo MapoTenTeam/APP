aa6bc468e935e61bb6c18a5cf1e8c0d883a28613
SoyoungLee <51827047+soyoung-dev@users.noreply.github.com>
2021-10-12 17:49:41 +0900

    암호화~ing

--- MapoTen/app/src/main/java/com/mapo/mapoten/ui/fragment/Login_01.kt
+++ MapoTen/app/src/main/java/com/mapo/mapoten/ui/fragment/Login_01.kt
@@ -22,8 +22,13 @@ import com.mapo.mapoten.system.AppPrefs
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.lang.Exception
+import java.lang.StringBuilder
 import java.security.DigestException
 import java.security.MessageDigest
+import java.math.BigInteger
+import java.nio.charset.Charset
+
 
 class Login_01 : Fragment() {
     private var _binding: FragmentLogin01Binding? = null
@@ -83,28 +88,63 @@ class Login_01 : Fragment() {
     }
 
     // 암호화
-    fun hashSHA256(msg: String) : String{
-        val hash: ByteArray
+    fun hashSHA256(msg: String): String {
+        var hash: ByteArray = msg.toByteArray()
+        val md = MessageDigest.getInstance("SHA-256")
+        Log.d("TAG","msg: $msg ByteArray :$hash")
         try {
-            val md = MessageDigest.getInstance("SHA-256")
-            md.update(msg.toByteArray())
-            md.update(salt.toByteArray())
+            for (i in 0..99999) {
+            val temp: String = msg + salt
+//            md.update(salt.toByteArray())
+//            md.update(msg.toByteArray())
+            md.update(temp.toByteArray())
             hash = md.digest()
+
+            }
+
         } catch (e: CloneNotSupportedException) {
             throw DigestException("couldn't make digest of partial content");
         }
-        return bytesToHex(hash)
+//        return bytesToHex(hash)
+        return Base64.encodeToString(hash, Base64.DEFAULT)
     }
 
-    fun bytesToHex(byteArray: ByteArray): String {
-        val hexChars = CharArray(byteArray.size * 2)
-        for (i in byteArray.indices) {
-            val v = byteArray[i].toInt() and 0xff
-            hexChars[i * 2] = digits[v shr 4]
-            hexChars[i * 2 + 1] = digits[v and 0xf]
+
+//    // 비밀번호 해싱
+//    @Throws(Exception::class)
+//    private fun hashing(password: ByteArray, salt: String): String {
+//        var password: ByteArray = password
+//        val md = MessageDigest.getInstance("SHA-256") // SHA-256 해시함수를 사용
+//
+//        // key-stretching
+//        for (i in 0..99999) {
+//            val temp: String =
+//                Byte_to_String(password).toString() + salt // 패스워드와 Salt 를 합쳐 새로운 문자열 생성
+//            md.update(temp.toByteArray()) // temp 의 문자열을 해싱하여 md 에 저장해둔다
+//            password = md.digest() // md 객체의 다이제스트를 얻어 password 를 갱신한다
+//        }
+//        return Base64.encodeToString(password, Base64.DEFAULT)
+////        return Byte_to_String(password)
+//    }
+
+    // 바이트 값을 16진수로 변경해준다
+    private fun Byte_to_String(temp: ByteArray): String {
+        val sb = StringBuilder()
+        for (a in temp) {
+            sb.append(String.format("%02x", a))
         }
-        return String(hexChars)
+        return sb.toString()
     }
+
+//    fun bytesToHex(byteArray: ByteArray): String {
+//        val hexChars = CharArray(byteArray.size * 2)
+//        for (i in byteArray.indices) {
+//            val v = byteArray[i].toInt() and 0xff
+//            hexChars[i * 2] = digits[v shr 4]
+//            hexChars[i * 2 + 1] = digits[v and 0xf]
+//        }
+//        return String(hexChars)
+//    }
 
     // 로그인
     private fun login() {
@@ -114,9 +154,8 @@ class Login_01 : Fragment() {
             var textId = idEditText.text.toString()
             var textPwd = pwdEditText.text.toString()
             Log.d("TAG", "클릭")
-            Log.d("TAG", "${hashSHA256(textPwd)}")
-//
-//            val base64Encoded = Base64.encodeToString(hashSHA256(textPwd),Base64.DEFAULT)
+//            Log.d("TAG", "해시 ${hashing(textPwd.toByteArray(), salt)}")
+            Log.d("TAG", "해시 ${hashSHA256(textPwd)}")
 
             val loginService = userService.requestLogin(LoginRequest(textId,hashSHA256(textPwd)))
             loginService.enqueue(object : Callback<LoginResponse> {
@@ -130,7 +169,8 @@ class Login_01 : Fragment() {
                             Log.d("TAG", "토큰 : ${response.body()?.accessToken}")
                         val token = response.body()?.accessToken
                             Log.d("TAG", "로그인 유저정보 : ${response.body()?.user_se}")
-                        Log.d("TAG", "비밀번호 ${hashSHA256(textPwd)}")
+//                        Log.d("TAG", "비밀번호 ${hashing(textPwd.toByteArray(), salt)}")
+//                        Log.d("TAG", "비밀번호 ${hashSHA256(textPwd)}")
                         if (token != null) {
                             AppPrefs.saveToken(requireActivity(), token)
                             AppPrefs.saveUserType(requireActivity(), response.body()!!.user_se)
@@ -142,6 +182,7 @@ class Login_01 : Fragment() {
                         Toast.makeText(context,
                                 "로그인 실패",
                                 Toast.LENGTH_SHORT).show()
+//                        Log.d("TAG", "비밀번호 ${hashing(textPwd.toByteArray(), salt)}")
                         Log.d("TAG", "비밀번호 ${hashSHA256(textPwd)}")
                         Log.d("TAG", "로그인 실패 ${response.code()} , ${response.message()}")
                     }
